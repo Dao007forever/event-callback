dbba2b208899f3f40e4eb9b53fbb051343693c9c
Le Trong Dao <dao@hoiio.com>
2013-09-10 17:05:58 +0800

    Fix config

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
-var config = {}
+var config = {};
 
+config.redis = {};
 config.redis.host = '';
 config.redis.port = ;
 config.redis.password = '';
+
+config.web = {};
 config.web.port = 3000;
 
 module.exports = config;
