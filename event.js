8d94b3fa8041048e919a7e6493ffccad293a10f8
Le Trong Dao <dao@hoiio.com>
2014-04-11 14:24:05 +0800

    Add comments

--- event.js
+++ event.js
@@ -4,8 +4,8 @@ var http = require("http");
 var Handler = require("./src/handler");
 var config = require("./config");
 
-// This auth_pass hasn't been released
-client = redis.createClient(config.redis.port, config.redis.host, {auth_pass: "hoiio123kid"});
+// Create only 1 client and login
+client = redis.createClient(config.redis.port, config.redis.host);
 client.auth(config.redis.password, function () {
     console.log("Logged in");
     client.setnx("counter", 0);
@@ -17,6 +17,7 @@ client.on("error", function (err) {
     console.log("Error " + err);
 });
 
+// Send QUIT command to Redis on exit
 process.on("exit", function() {
     client.quit();
     console.log("About to exit");
@@ -24,16 +25,19 @@ process.on("exit", function() {
 
 http.createServer(function (req, res) {
     req.content = "";
+    // Dealing with multipart
     req.addListener("data", function(chunk) {
+        // concat is faster than array join
         req.content += chunk;
         if (req.content.length > 1e6) {
+            // Return 413 if the request is too long
             response.writeHead(413, {'Content-Type': 'text/plain'}).end();
             req.connection.destroy();
         }
     });
 
     req.addListener("end", function() {
-        //parse req.content and do stuff with it
+        // parse req.content and do stuff with it
         var json;
         console.log(req.content);
         try {
